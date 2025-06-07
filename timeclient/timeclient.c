1b6c90098ab00a82f896ce8a37e4505720efe70b
Ivan <ivan19890418@gmail.com>
2025-06-07 12:33:22 +0700

    commit

--- timeclient/timeclient.c
+++ timeclient/timeclient.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
--- timeserver/timeserver.c
+++ timeserver/timeserver.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <strings.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<strings.h>
 #include<time.h>
 
 #define PORT        8080
